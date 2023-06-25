45641204f354fc9318ba55c64a02107b1193d182
Oscar Dartigues <oscardartigues92@gmail.com>
2023-06-25 16:45:01 +0200

    :lipstick: single-ingredient

--- Rattrapages-Dartigues-Oscar/06-Wordpress/Mongoo/wp-content/themes/scarotheme/single-ingredients.php
+++ Rattrapages-Dartigues-Oscar/06-Wordpress/Mongoo/wp-content/themes/scarotheme/single-ingredients.php
@@ -10,20 +10,26 @@ get_header();
 if (have_posts()) {
     while (have_posts()) {
         the_post();
+        $couleur = get_field('couleur');
         ?>
-        <h1><?php the_title(); ?></h1>
-        <div class="ingredient-content">
-            <?php the_content(); ?>
-            <div class="ingredient-content">
-                <?php
-                $couleur = get_field('couleur');
-                $prix = get_field('prix');
 
-                // Affiche les valeurs des champs ACF
-                echo '<p>Couleur : ' . $couleur . '</p>';
-                echo '<p>Prix : ' . $prix . '</p>';?>
+
+            <div class="ingredient-content <?php echo $couleur?>">
+                <div>
+                    <h1><?php the_title(); ?></h1>
+                    <?php
+
+                    $prix = get_field('prix');
+
+                    // Affiche les valeurs des champs ACF
+                    echo '<p>Couleur : ' . $couleur . '</p>';
+                    echo '<p>Prix : ' . $prix . '€</p>';?>
+                    <button class="addbtn">Ajoutez à mon menu</button>
+                </div>
+                <img class="salade" src="<?php echo get_theme_file_uri()?>/img/salade.webp">
             </div>
-        </div>
+
+
         <?php
     }
 } else {
